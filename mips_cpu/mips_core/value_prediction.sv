946908617991bdc174431dffbbaaa89fd58e589c
Pethaperumal Natarajan <pnataraj@ucsd.edu>
2024-02-26 17:27:44 -0800

    done untested value prediction

--- mips_cpu/mips_core/value_prediction.sv
+++ mips_cpu/mips_core/value_prediction.sv
@@ -17,11 +17,40 @@ module value_prediction #(
 // Last predicted
 logic [`DATA_WIDTH - 1 : 0] last_predicted;
 logic [`DATA_WIDTH - 1 : 0] predicted;
+logic [`ADDR_WIDTH - 1 : 0] last_addr;
+
+localparam HASH_WIDTH = 10;
+localparam HASH_NUM = 1 << HASH_WIDTH;
+logic lct [HASH_NUM];
+logic [`DATA_WIDTH - 1 : 0] lvpt [HASH_NUM];
+logic [HASH_WIDTH - 1 : 0] hash;
+logic [HASH_WIDTH - 1 : 0] last_hash;
+
+assign hash = addr[`ADDR_WIDTH - 1 : `ADDR_WIDTH - HASH_WIDTH];
+assign last_hash = last_addr[`ADDR_WIDTH - 1 : `ADDR_WIDTH - HASH_WIDTH];
 
 // Make prediction
 always_comb begin
-    // predict all zeros for now
-    predicted = '0;
+    if (lvpt[last_hash] == d_cache_data.data) begin
+        // if correct, set to predict
+        lct[last_hash] = 'b1;
+    end
+    else begin
+        // if incorrect, set to not predict
+        lct[last_hash] = 'b0;
+    end
+
+    lvpt[last_hash] = d_cache_data.data;         // d_cache data probably not in sync with last addr
+
+    if (lct[hash]) begin
+        // Predict with lvpt
+        predicted = lvpt[hash];
+    end
+    else begin
+        // do not predict
+        // predict zeros for now
+        predicted = '0;
+    end
 end
 
 // always_ff @( posedge clk ) begin
@@ -37,6 +66,7 @@ always_comb begin
     else if (vp_en) begin
         out = predicted;
         last_predicted = predicted;
+        last_addr = addr;
         en_recover = 'b0;
     end
 
