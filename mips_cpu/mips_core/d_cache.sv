ffa670180173e392fad6a387582718eb830deb2a
Pethaperumal Natarajan <pnataraj@ucsd.edu>
2024-03-12 18:59:12 -0700

    completed csd

--- mips_cpu/mips_core/d_cache.sv
+++ mips_cpu/mips_core/d_cache.sv
@@ -109,10 +109,12 @@ module d_cache #(
 	logic select_way;
 	logic r_select_way;
 	logic [DEPTH - 1 : 0] lru_rp;
-	logic [DEPTH - 1 : 0] mru_rp;
+	logic [DEPTH - 1 : 0] bip;
+	logic [1 : 0] bip_val [DEPTH];
 	logic [DEPTH - 1 : 0] used0;
 	logic [DEPTH - 1 : 0] used1;
 	logic [1 : 0] selector;
+	logic selector_en, bip_val_en;
 
 	// databanks
 	genvar g,w;
@@ -175,6 +177,14 @@ module d_cache #(
 	logic last_flush_word;
 	logic last_refill_word;
 
+	always @ (i_index) begin
+		if (i_index != 0) begin
+			// $display("enabling, i_index: %h", i_index);
+			bip_val_en = 1'b1;
+			selector_en = 1'b1;
+		end
+	end
+
 	always_comb
 	begin
 		tag_hit = ( ((i_tag == tagbank_rdata[0]) & valid_bits[0][i_index])
@@ -207,31 +217,47 @@ module d_cache #(
 				select_way = 'b1;
 			end
 			else begin
-				if (~i_index[1]) begin
-					if (i_index[0]) begin
-						// use mru
-						select_way = mru_rp[i_index];
-						if (selector != 2'b11)
-							selector = selector + 1;
+				if (i_index[1]) begin
+					
+					if (selector_en) begin
+						if (~i_index[0]) begin
+							// $display("selector: %b, i_index: %h", selector, i_index);
+							// use bip
+							// $display("using bip 1");
+							select_way = bip[i_index];
+							if (selector != 2'b11)
+								selector = selector + 1;
+						end
+						else begin
+							// $display("selector: %b, i_index: %h", selector, i_index);
+							// use lru
+							// $display("using lru 1");
+							select_way = lru_rp[i_index];
+							if (selector != 2'b00)
+								selector = selector - 1;
+						end
+						// $display("disabling selector");
+						selector_en = 0;
 					end
 					else begin
-						// use lru
-						select_way = lru_rp[i_index];
-						if (selector != 2'b00)
-							selector = selector - 1;
+						select_way = select_way;
+						selector = selector;
 					end
 				end
 				else begin
 					// use selector
-					if (~selector[1]) begin
+					if (selector[1]) begin
 						// use lru
+						// $display("using lru 2");
 						select_way = lru_rp[i_index];
 					end
 					else begin
-						// use mru
-						select_way = mru_rp[i_index];
+						// use bip
+						// $display("using bip 2");
+						select_way = bip[i_index];
 					end
 				end
+				// select_way = bip[i_index];
 			end
 		end
 		else
@@ -372,9 +398,10 @@ module d_cache #(
 				valid_bits[i] <= '0;
 			for (int i=0; i<DEPTH;i++) begin
 				lru_rp[i] <= 0;
-				mru_rp[i] <= 0;
 				used0[i] <= 0;
 				used1[i] <= 0;
+				bip[i] <= 0;
+				bip_val[i] <= 2'b00;
 			end
 			selector = 2'b10;
 		end
@@ -395,13 +422,32 @@ module d_cache #(
 						dirty_bits[select_way][i_index] <= 1'b1;
 					if (in.valid)
 					begin
-						// $display("select_way: %b", select_way);
 						if (select_way)
 							used1[i_index] <= 'b1;
 						else
 							used0[i_index] <= 'b1;
 						lru_rp[i_index] <= ~select_way;
-						mru_rp[i_index] <= select_way;
+						// $display("i_index: %h", i_index);
+						// if (hit)
+						// $display("hit");
+						// else
+						// $display("miss");
+						if (bip_val_en && (hit || bip_val[i_index] == 2'b11)) begin
+							bip[i_index] <= select_way;
+							bip_val[i_index] <= 2'b00;
+							bip_val_en = 1'b0;
+							// $display("1 bip[i_index]: %b, select_way: %b, i_index: %h", bip[i_index], select_way, i_index);
+						end
+						else if (bip_val_en)begin
+							bip[i_index] <= ~select_way;
+							bip_val[i_index] <= bip_val[i_index] + 1;
+							bip_val_en = 1'b0;
+							// $display("2 bip[i_index]: %b, select_way: %b, i_index: %h", bip[i_index], select_way, i_index);
+						end
+						else begin
+							bip[i_index] <= bip[i_index];
+							// $display("holding bip[i_index]");
+						end
 					end
 				end
 
